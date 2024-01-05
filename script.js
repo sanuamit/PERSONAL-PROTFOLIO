f0f431843de46ac9f67fa7f2dabbdea4e5637256
Amit Kumar Pati <121998394+sanuamit@users.noreply.github.com>
2024-01-05 12:20:14 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function () {
 
   // typing text animation script
   var typed = new Typed(".typing", {
-    strings: ["Front End Developer", "Web Developer", "Web Designer"],
+    strings: ["Full-Stack Developer", "Web Developer", "Web Designer"],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true,
   });
 
   var typed = new Typed(".typing-2", {
-    strings: ["Front End Developer", "Web Developer", "Web Designer"],
+    strings: ["Full-Stack Developer", "Web Developer", "Web Designer"],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true,
